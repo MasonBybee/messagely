a114ee330127d644b31f7e053c0cdb6163bb57ca
Mason Bybee <bybeemason@gmail.com>
2024-03-28 18:54:01 -0500

    fixed order of droptables

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     username text PRIMARY KEY,
@@ -7,8 +7,8 @@ CREATE TABLE users (
     first_name text NOT NULL,
     last_name text NOT NULL,
     phone text NOT NULL,
-    join_at timestamp without time zone NOT NULL,
-    last_login_at timestamp with time zone
+    join_at Timestamp with time zone NOT NULL,
+    last_login_at Timestamp with time zone
 );
 
 CREATE TABLE messages (
@@ -16,6 +16,7 @@ CREATE TABLE messages (
     from_username text NOT NULL REFERENCES users,
     to_username text NOT NULL REFERENCES users,
     body text NOT NULL,
-    sent_at timestamp with time zone NOT NULL,
-    read_at timestamp with time zone
+    sent_at Timestamp with time zone NOT NULL,
+    read_at Timestamp with time zone
 );
+
